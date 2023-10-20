110d34c706553b1219c01b4643f6edd5a6b8c31b
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-10-20 12:51:27 -0300

    feat: create coffees section

---
+++ src/pages/Home/sections/Coffees/index.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '../../../../components/Heading';
+import * as S from './styles';
+
+export function CooffeesSection() {
+  return (
+    <S.CoffeesSection>
+      <Heading $level="h2">Nossos cafés</Heading>
+    </S.CoffeesSection>
+  );
+}
