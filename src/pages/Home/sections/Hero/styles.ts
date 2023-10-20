365ff9ab0fd95ecce963048c8c122da616dcbbd0
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-10-20 19:20:25 -0300

    refactor: remove heroItems

--- src/pages/Home/sections/Hero/styles.ts
+++ src/pages/Home/sections/Hero/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { HeroItemBackground } from '.';
 
 export const HeroContext = styled.div`
   flex: 1.2;
@@ -37,39 +36,4 @@ export const HeroImage = styled.div`
       width: 29.75rem;
     }
   }
-`;
-
-export const HeroItems = styled.div``;
-
-interface ItemIconStyles {
-  $bg: HeroItemBackground;
-}
-
-export const ItemIcon = styled.div<ItemIconStyles>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  width: 2rem;
-  height: 2rem;
-
-  border-radius: 100px;
-
-  color: ${({ theme }) => theme.colors.base.white};
-
-  ${(props) => {
-    const { $bg } = props;
-    switch ($bg) {
-      case 'primary':
-        return `background-color: ${props.theme.colors.yellow.normal}`;
-      case 'secondary':
-        return `background-color: ${props.theme.colors.yellow.dark}`;
-      case 'purple':
-        return `background-color: ${props.theme.colors.purple.normal}`;
-      case 'dark':
-        return `background-color: ${props.theme.colors.base.text}`;
-      default:
-        return ``;
-    }
-  }}
 `;
