51afb56b72468843493ba6a464086a6fa6352362
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-10-20 12:49:13 -0300

    feat: add background to hero section

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,11 +3,19 @@ import Background from '../../assets/background.png';
 
 export const Main = styled.main`
   height: calc(100vh - 6.5rem);
+  margin-top: 6.5rem;
 
-  section {
-    background: url(${Background}) no-repeat top center;
+  section:first-child {
+    padding-top: 5.75rem;
+    height: 34rem;
+
+    background:
+      linear-gradient(
+        0deg,
+        rgba(250, 250, 250, 1) 0%,
+        rgba(255, 255, 255, 0) 10%
+      ),
+      url(${Background}) no-repeat;
     background-size: cover;
   }
 `;
-
-export const Section = styled.section``;
